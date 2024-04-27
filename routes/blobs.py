35f5acfed67cca0a28d23f30f5d46193c452e48c
Jorge Perez <jorgeperez@MX2-1031452MBP.domain.name>
2024-04-26 22:41:17 -0600

    modified blobs files

--- routes/blobs.py
+++ routes/blobs.py
@@ -4,7 +4,8 @@ from azure_blob_functions.blob import upload_blob
 blob_routes = APIRouter()
 
 @blob_routes.post("/upload")
-def upload (container: str = Form(...), file: UploadFile = File(...)):
-    data = file.read()
+async def upload (container: str = Form(...), file: UploadFile = File(...)):
+    #data = await file.read()
+    data_path = file
     filename = file.filename
-    return upload_blob(filename, container, data)
+    return upload_blob(filename, container, data_path)
