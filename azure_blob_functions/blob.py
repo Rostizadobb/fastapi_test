257ac7f39ece3b1c4147cab58fc66620586ab7e9
Jorge Perez <jorgeperez@MX2-1031452MBP.domain.name>
2024-04-27 15:27:19 -0600

    file WORK

--- azure_blob_functions/blob.py
+++ azure_blob_functions/blob.py
@@ -1,6 +1,6 @@
 #from typing import BinaryIO
 import io
-from azure.storage.blob import BlobServiceClient, BlobType
+from azure.storage.blob import BlobServiceClient, BlobBlock
 import os
 import uuid
 from responses_colection.response_json import response_json
@@ -12,27 +12,22 @@ def upload_blob(filename: str, container: str, file):
     try:
         blob_client = blob_service_client.get_blob_client(container = container, blob = filename)
         # upload data
+        block_list=[]
+        chunk_size=1024*1024*4
         with file.file as f:
-            chunk_size=500 * 1024 * 1024
-            total_length = 0
-            chunks = []
             while True:
-                chunk = f.read(chunk_size)
-                if not chunk:
-                    break
-                chunks.append(chunk)
-                total_length += len(chunk)
-
-            for i, chunk in enumerate(chunks):
-                blob_client.upload_blob(data=chunk, blob_type=BlobType.BlockBlob, length=total_length)
-
-        # Fetch the blob properties to get the file size
-        blob_properties = blob_client.get_blob_properties()
-        file_size = blob_properties['size']
-        message = f"File uploaded successfully. File size: {file_size} bytes"
+                read_data = f.read(chunk_size)
+                if not read_data:
+                    break # done
+                blk_id = str(uuid.uuid4())
+                blob_client.stage_block(block_id=blk_id,data=read_data) 
+                block_list.append(BlobBlock(block_id=blk_id))
+        blob_client.commit_block_list(block_list)
+        message = "File uploaded successfully."
         return response_json(message)
-    except Exception as e:
-        return response_json(message=e.message, status = 500)
+    except BaseException as e:
+        message = "ERROR File not processed."
+        return response_json(message , status = 500)
             
 
 
