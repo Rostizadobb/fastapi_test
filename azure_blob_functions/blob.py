efc0c54de6bbc79537bf9bc4836c0c56fbcefdac
Jorge Perez <jorgeperez@MX2-1031452MBP.domain.name>
2024-04-28 16:31:14 -0600

    Added parquet to csv transformation

--- azure_blob_functions/blob.py
+++ azure_blob_functions/blob.py
@@ -5,7 +5,8 @@ from responses_colection.response_json import response_json
 AZURE_STORAGE_CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=azureyellowcab;AccountKey=tlRAtEUmclKrQPupGAnwlo6GyivJyVLI2DzsAlsDxCYbGx1896Jhsr6MOHShABk+ukTMsLSnUclt+AStgk6vww==;EndpointSuffix=core.windows.net'
 blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
 
-def upload_blob(filename: str, container: str, file):
+def upload_blob(filename: str, file):
+    container = "cabfiles"
     try:
         blob_client = blob_service_client.get_blob_client(container = container, blob = filename)
         # Upload data by chunks
--- routes/blobs.py
+++ routes/blobs.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter, Form, UploadFile, File
 from azure_blob_functions.blob import upload_blob
+import io
+from fastapi import APIRouter, UploadFile, File, HTTPException
+import pandas as pd
+
 
 blob_routes = APIRouter()
 
 @blob_routes.post("/upload")
 async def upload (file: UploadFile = File(...)):
-    filename = file.filename
-    container = "cabfiles"
-    return upload_blob(filename, container, file)
+    if not file.filename.endswith(".parquet"):
+        raise HTTPException(status_code=400, detail="Uploaded file must be in .parquet format")
+    parquet_data = pd.read_parquet(io.BytesIO(await file.read()))
+    csv_data = parquet_data.to_csv(index=False)
+    converted_file = io.BytesIO(csv_data.encode())
+    csv_file = UploadFile(filename=file.filename.replace(".parquet", ".csv"), file=converted_file, media_type="text/csv")
+    filename = csv_file.filename
+    return upload_blob(filename, csv_file)
