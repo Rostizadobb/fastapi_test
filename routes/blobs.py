bc3c654e1726361f543211f45f30e8f458249b62
Jorge Perez <jorgeperez@MX2-1031452MBP.domain.name>
2024-04-28 17:03:49 -0600

    Cleanup

--- routes/blobs.py
+++ routes/blobs.py
@@ -1,7 +1,5 @@
 from azure_blob_functions.blob import upload_blob
-import io
 from fastapi import APIRouter, UploadFile, File, HTTPException
-import pandas as pd
 
 
 blob_routes = APIRouter()
@@ -10,9 +8,5 @@ blob_routes = APIRouter()
 async def upload (file: UploadFile = File(...)):
     if not file.filename.endswith(".parquet"):
         raise HTTPException(status_code=400, detail="Uploaded file must be in .parquet format")
-    parquet_data = pd.read_parquet(io.BytesIO(await file.read()))
-    csv_data = parquet_data.to_csv(index=False)
-    converted_file = io.BytesIO(csv_data.encode())
-    csv_file = UploadFile(filename=file.filename.replace(".parquet", ".csv"), file=converted_file, media_type="text/csv")
-    filename = csv_file.filename
-    return upload_blob(filename, csv_file)
+    filename = file.filename
+    return upload_blob(filename, file)
